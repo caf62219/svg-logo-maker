6415c122d80aacafae94caad3e3001823cb8b170
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-08-19 17:01:40 -0700

    updates to the CLI file to write the svg

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -1,10 +1,11 @@
 const Triangle = require ('../lib/triangle');
 
+
 describe ('Triangle', () => {
     describe ('render', () => {
         test('should render triangle with a fill color', ()=> {
-            const shapeColor='green';
-            const expectedSvg = `<triangle points="150, 18 244, 182 56, 182" fill="${shapeColor}" />`;
+            shapeColor='green';
+            const expectedSvg = `<triangle points="150, 18 244, 182 56, 182" fill="${shapeColor}"/>`;
 
             const triangle = new Triangle();
 
